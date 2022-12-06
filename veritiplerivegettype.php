4bc979a9f5156bb50239cf2b98f715ac78d7dea6
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-06 22:32:48 +0300

    Create veritiplerivegettype.php

---
+++ veritiplerivegettype.php
@@ -0,0 +1,12 @@
+<?php
+define("NET", 0); // sabit
+$hello_int = 4; // sayısal
+$hello_float = 5.2; // kesirli
+$hello_string = "Merhaba";  // string
+$hello_bool = true;  // boolean
+$hello_NULL = NULL; // Boş değer
+echo $hello_int;
+echo gettype($hello_NULL); // Veri tipinin ne olduğuna bakar.
+echo NET;
+echo "Finish" // Sona ";" koyulması gerekmez, kapatma işlemi bu işi görür.
+?>
