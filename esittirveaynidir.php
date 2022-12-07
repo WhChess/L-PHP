095a6051b77a38b17a8125d9192e74d3edf1095c
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-07 20:42:07 +0300

    Create esittirveaynidir.php

---
+++ esittirveaynidir.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * "===" VERİ TİPLERİ DAHİL EŞİT DEMEK.
+ * "==" SADECE GÖRÜNÜŞLERİ EŞİT OLSA YETERLİDİR.
+ *
+ */
+$x = "2";
+$y = 2;
+var_dump($x==$y); // True
+var_dump($x===$y); // False
