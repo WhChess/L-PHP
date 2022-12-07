092c377d854ca1e73ba4b1750fafc9187847ac30
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-07 21:03:44 +0300

    Create spaceship.php

---
+++ spaceship.php
@@ -0,0 +1,8 @@
+<?php
+// Spaceship operatörü
+// INTEGER değer döndürür.
+// Diğer operatörlerden farklıdır.
+echo 1 <=> 1; //output  0 - Eşitse 0
+echo 3 <=> 4; //output -1 - Sağdaki büyükse -1
+echo 4 <=> 3; //output  1 - Soldaki büyükse 1
+// değerlerini döndürür.
