d2bae2163f4bb3f026228c342dffd683b8a1572f
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-20 23:13:42 +0300

    Create oncedenartirma.php

---
+++ oncedenartirma.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = 0;
+echo ++$a; # önceden 1 artırma, 1 değeri bastırılır.
+echo "\n";
+echo $a++; # sonradan 1 artırma, 1 değeri bastırılır, bastırıldıktan sonra değer 1 artar. a = 2
+echo "\n";
+echo --$a; # önceden 1 eksiltme, 1 değeri bastırılır.
+echo "\n";
+echo $a--; # sonradan 1 eksiltme, 1 değeri bastırılır, bastırıldıktan sonra değer 1 azalır. a = 0
