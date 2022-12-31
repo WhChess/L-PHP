34d82756e7327ee40f1217847d5821d80f00c503
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-31 18:14:41 +0300

    Create veridonusumleri.php

---
+++ veridonusumleri.php
@@ -0,0 +1,10 @@
+<?php
+
+$age = (int)'<script>zararlı kod</script>';
+echo "$age \n";
+$sayi = (float)'2023.1 Hoşgeldin!';
+echo "$sayi \n";
+$sayi2 = (float)'Hoşgeldin 2024.2!';
+echo "$sayi2";
+
+?>
