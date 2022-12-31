09a59f1067addcc5b26af83b8acf1d6b0b99cd56
A.Efe Anik <66623661+WhChess@users.noreply.github.com>
2022-12-31 18:23:46 +0300

    Create veridonusumu-bool-str.php

---
+++ veridonusumu-bool-str.php
@@ -0,0 +1,10 @@
+<?php
+
+$bool1 = (string) true; // true = 1
+$bool2 = (string) false; // false = 0
+
+var_dump($bool1); // "1"
+var_dump($bool2); // ""
+// false değeri "boş dizge", true değeri "1"'e dönüşür.
+
+?>
